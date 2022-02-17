5f6043980dd5530b6514324be06ecef109479464
Rohit Sharma <RohitShrm627@gmail.com>
2022-02-17 17:14:31 +0530

    Add project files.

---
+++ Brainstorm Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Brainstorm_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
